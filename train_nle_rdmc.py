bcfe01d1c97f43b3ea673dfefea97e85a2421c3d
maltelueken <malte_lueken@arcor.de>
2025-11-07 17:18:30 +0100

    Lower learning rate for NLE RDMC training

--- train_nle_rdmc.py
+++ train_nle_rdmc.py
@@ -90,7 +90,7 @@ approximator = bf.ContinuousApproximator(
 epochs = 100
 num_batches = 500
 batch_size = 64
-learning_rate = keras.optimizers.schedules.CosineDecay(5e-4, decay_steps=epochs*num_batches, alpha=1e-6)
+learning_rate = keras.optimizers.schedules.CosineDecay(5e-5, decay_steps=epochs*num_batches, alpha=1e-6)
 optimizer = keras.optimizers.Adam(learning_rate=learning_rate, clipnorm=1.0)
 approximator.compile(optimizer=optimizer)
 
@@ -99,5 +99,5 @@ history = approximator.fit(
     num_batches=num_batches,
     batch_size=batch_size,
     simulator=simulator,
-    callbacks=[keras.callbacks.ModelCheckpoint("rdmc_nle_spline_student_deep.keras", monitor="loss", mode="min", save_best_only=True)]
+    callbacks=[keras.callbacks.ModelCheckpoint("checkpoints/rdmc_nle_spline_student_deep_low_lr_100.keras", monitor="loss", mode="min", save_best_only=True)]
 )
