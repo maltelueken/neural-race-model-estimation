5952e16d579b10dd92ddb991f69d03bdd16398f7
maltelueken <malte_lueken@arcor.de>
2026-01-02 15:34:17 +0100

    Add NPE for RDMC with two accumulators

--- src/confrdm/simulators/rdmc.py
+++ src/confrdm/simulators/rdmc.py
@@ -185,3 +185,21 @@ def create_nle_adapter_single_accumulator(param_names):
         .concatenate(param_names, into="inference_conditions")
         .rename("x", "inference_variables")
     )
+
+
+def create_npe_adapter_two_accumulators(param_names):
+    return (
+        bf.Adapter()
+        .to_array()
+        .convert_dtype("float64", "float32")
+        .broadcast("num_obs", to="x", exclude=(-2, -1), squeeze=-1)
+        .as_set(["x"])
+        .log(param_names)
+        .sqrt("num_obs")
+        .concatenate(param_names, into="inference_variables")
+        .concatenate(["x"], into="summary_variables")
+        .rename("num_obs", "inference_conditions")
+        .keep(
+            ["inference_variables", "inference_conditions", "summary_variables"]
+        )
+    )
