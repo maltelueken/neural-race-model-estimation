7f22f97e57052cfe478467ff4e57545f0e235c90
maltelueken <malte_lueken@arcor.de>
2026-02-01 17:31:34 +0100

    Fix bug in CRDM prior

--- src/confrdm_jax/priors.py
+++ src/confrdm_jax/priors.py
@@ -10,7 +10,7 @@ def scale_logtruncnorm(x, loc, scale, a):
 @jax.jit
 def rdm_prior(x):
     x = jnp.exp(x)
-    v_intercept = scale_logtruncnorm(x[0], 1.0, 0.5, 0.0)
+    v_intercept = scale_logtruncnorm(x[0], 1.0, 0.25, 0.0)
     v_slope = scale_logtruncnorm(x[1], 4.0, 0.5, 0.0)
     s_true = stats.gamma.logpdf(x[2], a=12.0, scale=0.1)
     b = stats.gamma.logpdf(x[3], a=8.0, scale=0.15)
@@ -21,11 +21,11 @@ def rdm_prior(x):
 @jax.jit
 def crdm_prior(x):
     x = jnp.exp(x)
-    v_intercept = scale_logtruncnorm(x[0], 1.0, 0.5, 0.0)
+    v_intercept = scale_logtruncnorm(x[0], 1.0, 0.25, 0.0)
     v_slope = scale_logtruncnorm(x[1], 4.0, 0.5, 0.0)
     amp = scale_logtruncnorm(x[2], 0.3, 0.05, 0.0)
     tau = scale_logtruncnorm(x[3], 0.1, 0.05, 0.0)
-    s_true = stats.gamma.logpdf(x[2], a=12.0, scale=0.1)
+    s_true = stats.gamma.logpdf(x[4], a=12.0, scale=0.1)
     b = stats.gamma.logpdf(x[5], a=4.0, scale=0.15)
     t0 = scale_logtruncnorm(x[6], 0.3, 0.2, 0.0)
 
