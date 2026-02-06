0c1eb657e8a86e5feb7d6d733dcff3b24ac854df
maltelueken <malte_lueken@arcor.de>
2026-02-06 09:30:03 +0100

    Fix RDM parameter recovery slurm script

--- slurm/parameter_recovery_rdm.sh
+++ slurm/parameter_recovery_rdm.sh
@@ -15,7 +15,7 @@ module load 2023
 
 source bin/activate
 
-python scripts/parameter_recovery_rdm.py \
+python scripts/parameter_recovery.py \
     model=rdm \
     model.num_bins=12 \
     model.num_mid=128 \
